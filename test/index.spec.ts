055dcd964e5ee48bb2cc80e5fcafaed9c847f452
RayhanHamada <hamadadev2016@gmail.com>
2020-09-22 16:56:31 +0700

    add test and modify husky config

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,8 +1,12 @@
 import { expect } from 'chai';
-import { actionTut } from '../src';
+import { actionTut, actionTut2 } from '../src';
 
 describe('index.js', function () {
   it('actionTut function should return a string', function () {
     expect(actionTut()).is.a('string');
+  });
+
+  it('actionTut2 function should return "Something Awesome"', function () {
+    expect(actionTut2()).to.be.eq('Something Awesome');
   });
 });
